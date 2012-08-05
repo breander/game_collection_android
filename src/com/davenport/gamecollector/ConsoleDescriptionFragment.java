67cd0b01dcb1c600c58c37243f8ad52ba4513b0a
breander <breander@gmail.com>
2012-08-05 19:39:59 -0400

    improved load times, changed console description to web view

--- src/com/davenport/gamecollector/ConsoleDescriptionFragment.java
+++ src/com/davenport/gamecollector/ConsoleDescriptionFragment.java
@@ -12,9 +12,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.os.AsyncTask;
+import android.webkit.WebView;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import android.graphics.*;
 
 public class ConsoleDescriptionFragment extends Fragment implements LoaderCallbacks<Void> {
 	private Platform _platform;
+	String APIKEY = "?api_key=90fbab5cb7aa63ea95938c3a39f19d2049308e40";
+	String Website = "http://api.giantbomb.com";
 	
 	ConsoleDescriptionFragment(){
 		
@@ -45,21 +59,8 @@ public class ConsoleDescriptionFragment extends Fragment implements LoaderCallba
 			}
 		}
 		
-		TextView desc = (TextView) getView().findViewById(R.id.descriptionText);
-		if(desc != null)
-		{
-			if(_platform != null)
-			{
-				Spanned marked_up = Html.fromHtml(_platform.description);
-				desc.setMovementMethod(new ScrollingMovementMethod());
-				desc.scrollTo(0, 0);
-				desc.setText(marked_up);
-			}
-			else
-			{
-				desc.setText("Null");
-			}
-		}
+		DownloadWebPageTask task = new DownloadWebPageTask();
+		task.execute(new String[] { Website + "/platform/" + _platform.id + "/" + APIKEY + "&field_list=description&format=xml" });
 	}
 
 	@Override
@@ -85,5 +86,93 @@ public class ConsoleDescriptionFragment extends Fragment implements LoaderCallba
 	public void onLoaderReset(Loader<Void> arg0) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private class DownloadWebPageTask extends AsyncTask<String, Void, String>  {
+		@Override
+		protected String doInBackground(String... urls) {
+			String response = "";
+
+	        for (String url : urls) {
+	        	DefaultHttpClient client = new DefaultHttpClient();
+	        	HttpGet httpGet = new HttpGet(url);
+	        	try {
+	        		HttpResponse execute = client.execute(httpGet);
+	        		InputStream content = execute.getEntity().getContent();
+
+	        		BufferedReader buffer = new BufferedReader(
+						new InputStreamReader(content));
+	        		String s = "";
+	        		while ((s = buffer.readLine()) != null) {
+	        			response += s;
+
+
+	        		}
+
+	        	} catch (Exception e) {
+	        		e.printStackTrace();
+	        	}
+	        }
+			return response;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			
+			WebView desc = (WebView) getView().findViewById(R.id.descriptionText);
+			if(desc != null)
+			{
+				if(_platform != null)
+				{
+					//result = result.replace("OK1110","W");
+					
+					
+					
+					//Spanned marked_up = Html.fromHtml(result);
+					//desc.setMovementMethod(new ScrollingMovementMethod());
+					desc.setBackgroundColor(Color.BLACK);
+					desc.scrollTo(0, 0);
+					desc.loadData(result, "text/html", null);
+					//desc.setText(marked_up);
+					//desc.setText(result);
+					//desc.setBackgroundColor(Color.parseColor("#000000"));
+					//desc.setBackgroundColor(Color.GRAY);
+					desc.setFocusableInTouchMode(false);
+					desc.setFocusable(false);
+					
+				}
+				else
+				{
+					//desc.setText("Null");
+				}
+			}
+			return;
+			
+			//ParsePlatforms sp = new ParsePlatforms(result);
+			//sp.parseDocument();
+
+			//for(Platform plat : sp.ReturnList()){
+
+				//if(plat == null)
+				//{
+					//TextView desc = (TextView) getView().findViewById(R.id.descriptionText);
+					//if(desc != null)
+					//{
+						//if(_platform != null)
+						//{
+							//Spanned marked_up = Html.fromHtml(plat.description);
+							//desc.setMovementMethod(new ScrollingMovementMethod());
+							//desc.scrollTo(0, 0);
+							//desc.setText(marked_up);
+						//}
+						//else
+						//{
+							//desc.setText("Null");
+						//}
+					//}
+					//break;
+				//}
+			//}
+		}
 	}
 }
